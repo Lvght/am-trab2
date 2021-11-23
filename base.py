0dae8baae3307d35671aa9c2aee4daf58bea0925
lucas-cid <lucas_mcid@hotmail.com>
2021-11-23 12:56:55 -0300

    Agrupamento Hierárquico

--- base.py
+++ base.py
@@ -10,6 +10,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram
+from matplotlib import pyplot as plt
+
 import csv
 
 file = open('datasets/student_data_por.csv')
@@ -32,3 +36,26 @@ print('Len is ', len(X))
 print('Instance size is ', X[0].size)
 
 # Implemente aqui o seu algoritmo.
+
+# Agrupamento Hierárquico
+agglomerative = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+model = agglomerative.fit(X)
+
+counts = np.zeros(model.children_.shape[0])
+n_samples = len(model.labels_)
+for i, merge in enumerate(model.children_):
+    current_count = 0
+    for child_idx in merge:
+        if child_idx < n_samples:
+            current_count += 1  # leaf node
+        else:
+            current_count += counts[child_idx - n_samples]
+    counts[i] = current_count
+
+linkage_matrix = np.column_stack(
+    [model.children_, model.distances_, counts]
+).astype(float)
+
+dendrogram(linkage_matrix, truncate_mode="level", p=3)
+
+
