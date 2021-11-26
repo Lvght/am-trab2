66d2d4d56bcd3eaf4e927b14c8097ecd5203c4df
lucas-cid <lucas_mcid@hotmail.com>
2021-11-26 08:14:40 -0300

    Dataset change and minor fixes

--- base.py
+++ base.py
@@ -12,24 +12,17 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster.hierarchy import dendrogram
+from sklearn.metrics import silhouette_score
 from matplotlib import pyplot as plt
-
 import csv
 
-file = open('datasets/student_data_por.csv')
+file = open('tripadvisor_review.csv')
 csvreader = csv.reader(file)
 header = next(csvreader)
 data = []
 
 for row in csvreader:
-  data.append(row)
-
-
-enc = OneHotEncoder(handle_unknown='ignore');
-X = enc.fit_transform(data).toarray()
-
-scaler = StandardScaler(with_mean=False).fit(X)
-X = scaler.transform(X)
+  data.append(row[1:])
 
 print(X)
 print('Len is ', len(X))
@@ -38,16 +31,17 @@ print('Instance size is ', X[0].size)
 # Implemente aqui o seu algoritmo.
 
 # Agrupamento Hierárquico
-agglomerative = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
-model = agglomerative.fit(X)
+agglomerative = AgglomerativeClustering(distance_threshold=20, n_clusters=None)
+model = agglomerative.fit(data)
 
 counts = np.zeros(model.children_.shape[0])
 n_samples = len(model.labels_)
+
 for i, merge in enumerate(model.children_):
     current_count = 0
     for child_idx in merge:
         if child_idx < n_samples:
-            current_count += 1  # leaf node
+            current_count += 1  
         else:
             current_count += counts[child_idx - n_samples]
     counts[i] = current_count
@@ -57,5 +51,4 @@ linkage_matrix = np.column_stack(
 ).astype(float)
 
 dendrogram(linkage_matrix, truncate_mode="level", p=3)
-
-
+silhouette_score(data, model.labels_)
